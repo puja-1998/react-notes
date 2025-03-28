b8cbced56d309cea0ede4d4c140b7fbbd56aaf5b
Puja-1998 <ghodkepooja98@gmail.com>
2025-03-28 12:10:16 +0530

    updated

--- state-and-setstate/src/App.js
+++ state-and-setstate/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [str, setStr] = useState("Change the text");
+  
+
+  function handlebutton(){
+    setStr("Text change to :Hello Puja");
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Change Me using a button</h1>
+        <button onClick={handlebutton}>{str}</button>
       </header>
     </div>
   );
