c61bfdc4a42e723f39385ced299f80b5d09b3e7d
Puja-1998 <ghodkepooja98@gmail.com>
2025-03-28 11:29:55 +0530

    Worked on  stateand setState

--- state-and-setstate/src/index.js
+++ state-and-setstate/src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CounterClass from './CounterClass';
-import CounterFunction from './CounterFunction';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(<CounterFunction/>)
- root.render(
+root.render(
   <React.StrictMode>
-    <CounterFunction/><hr/>
-    <CounterClass/><hr/>
+    <App />
   </React.StrictMode>
- );
+);
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
